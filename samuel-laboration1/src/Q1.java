b36ca5bc049c6119ee00b1420a4692a7d16a8dd2
Samuel Thand <sath2102@student.miun.se>
2023-03-22 15:16:23 +0100

    loaded maze for Q1, started building stack

--- samuel-laboration1/src/Q1.java
+++ samuel-laboration1/src/Q1.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Q1 {
+
+    public static int[][] loadMaze(String file) throws FileNotFoundException {
+        var mazeInput = new File(file);
+
+        var scanner = new Scanner(mazeInput);
+        int rows = 0;
+        int cols = 0;
+        while (scanner.hasNextLine()) {
+            cols = scanner.nextLine().split(" ").length;
+            rows++;
+        }
+
+        if (rows != cols)
+            throw new IllegalArgumentException("The file must have an equal amounts of rows and columns!");
+        else {
+            scanner = new Scanner(mazeInput);
+            int[][] maze = new int[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                var row = scanner.nextLine().split(" ");
+
+                for (int j = 0; j < cols; j++) {
+                    maze[i][j] = Integer.parseInt(row[j]);
+                }
+            }
+
+            return maze;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("running");
+        var inputFile = "samuel-laboration1/Q1-input.txt";
+        try {
+            int[][] maze = loadMaze(inputFile);
+
+        } catch (FileNotFoundException | IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
