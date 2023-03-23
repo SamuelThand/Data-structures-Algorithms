99d6f59441895e6c6a7f753328fa30736b2467e1
Samuel Thand <sath2102@student.miun.se>
2023-03-23 17:26:07 +0100

    Finished laboration2

--- samuel-laboration1/src/Q2.java
+++ samuel-laboration1/src/Q2.java
@@ -1,5 +1,36 @@
+import java.util.EmptyStackException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Q2 {
     public static void main(String[] args) {
-        System.out.println("running2");
+        var scanner = new Scanner(System.in);
+        System.out.println("Enter a string with an expression of nested parenthesis:");
+        String expression = scanner.nextLine();
+        scanner.close();
+
+        SamuelFixedSizeStack<Object> currentDepth = new SamuelFixedSizeStack<>(expression.length());
+
+        int maxDepth = 0;
+        try {
+            for (int i = 0; i < expression.length(); i++) {
+                if (expression.charAt(i) == '(')
+                    currentDepth.push(new Object());
+                    if (currentDepth.size() > maxDepth)
+                        maxDepth++;
+
+                else if (expression.charAt(i) == ')')
+                    currentDepth.pop();
+            }
+
+            if (currentDepth.size() != 0)
+                throw new InputMismatchException();
+
+            System.out.printf("The currentDepth of nested parenthesis: %d", maxDepth);
+        } catch (EmptyStackException e) {
+            System.out.println("The expression has more right parenthesis then left parenthesis");
+        } catch (InputMismatchException e) {
+            System.out.println("The expression has more left parenthesis then right parenthesis");
+        }
     }
 }
