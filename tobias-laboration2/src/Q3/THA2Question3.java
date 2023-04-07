a279711655b18ac6da127193c0e46d4fef9bcc06
toha2102 <toha2102@student.miun.se>
2023-04-07 09:58:56 +0200

    Part a demo fin

--- tobias-laboration2/src/Q3/THA2Question3.java
+++ tobias-laboration2/src/Q3/THA2Question3.java
@@ -7,6 +7,21 @@ public class THA2Question3 {
     public static void main(String[] args) {
         THBinarySearchTree<Character> tree = new THBinarySearchTree<>();
 
+        insert(tree, "123");
+        tree.print();
+
+        System.out.printf("The tree is balanced: %b\n", tree.checkBalance());
+
+        tree.remove('1');
+        tree.insert('1');
+        tree.print();
+        System.out.printf("The tree is balanced: %b\n", tree.checkBalance());
+
+        System.out.printf("Char 1 is in the tree: %b\n", tree.find('1'));
+        System.out.printf("Char 4 is in the tree: %b\n", tree.find('4'));
+
+
+
     }
 
     private static void insert(THBinarySearchTree<Character> tree, String word) {
