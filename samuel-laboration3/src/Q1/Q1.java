959a2d10f2d2b7965512cbd06ecd0826e15a621b
Samuel Thand <sath2102@student.miun.se>
2023-04-24 14:47:24 +0200

    Finished question 1

--- samuel-laboration3/src/Q1/Q1.java
+++ samuel-laboration3/src/Q1/Q1.java
@@ -1,43 +1,78 @@
 package Q1;
 
-import SamuelDatastructures.BSTNode;
+import SamuelDatastructures.BinaryTreeNode;
 
 public class Q1 {
 
     public static void main(String[] args) {
 
-//
-//        var tree = new BSTNode<>(97);
-//
-//        tree.insert(46);
-//        tree.insert(12);
-//        tree.insert(6);
-//        tree.insert(9);
-//        tree.insert(3);
-//        tree.insert(37);
-//        tree.insert(38);
-//        tree.insert(31);
-//        tree.insert(32);
-//
-//        tree.print();
+        var tree1 = new BinaryTreeNode<>(50);
 
-//        TODO Maxheap kan inte byggas med denna datastrukturen, gör en modifierad som kan
+        tree1.insert(48);
+        tree1.insert(52);
+        tree1.insert(46);
+        tree1.insert(51);
+        tree1.insert(53);
+        System.out.println("The tree is not complete and each node is not >= its child");
+        tree1.print();
 
+        System.out.printf("Is a complete tree: %b\n", tree1.isCompleteTree());
+        System.out.printf("Has the max-heap property for all nodes: %b\n", tree1.hasMaxHeapProperty());
+        System.out.printf("Is a max-heap: %b\n", tree1.isMaxHeap());
+        System.out.println();
+        System.out.println();
 
 
-        var tree = new BSTNode<>(50);
+        var tree2 = new BinaryTreeNode<>(50);
 
-        tree.insert(48);
-        tree.insert(52);
-        tree.insert(46);
-        tree.insert(49);
-        tree.insert(51);
-        tree.insert(53);
-        System.out.println("Not a max heap, the tree is complete but each node is not >= its child");
-        tree.print();
-        System.out.println(tree.isMaxHeap());
+        tree2.insert(48);
+        tree2.insert(52);
+        tree2.insert(46);
+        tree2.insert(49);
+        tree2.insert(51);
+        tree2.insert(53);
+        System.out.println("The tree is complete but each node is not >= its child");
+        tree2.print();
 
+        System.out.printf("Is a complete tree: %b\n", tree2.isCompleteTree());
+        System.out.printf("Has the max-heap property for all nodes: %b\n", tree2.hasMaxHeapProperty());
+        System.out.printf("Is a max-heap: %b\n", tree2.isMaxHeap());
+        System.out.println();
+        System.out.println();
 
+        var tree3 = new BinaryTreeNode<>(53);
+
+        tree3.insert(52);
+        tree3.insert(51);
+        tree3.insert(50);
+        tree3.insert(49);
+        tree3.insert(48);
+        tree3.insert(46);
+        System.out.println("Each node is >= its child, but the tree is not complete");
+        tree3.print();
+
+        System.out.printf("Is a complete tree: %b\n", tree3.isCompleteTree());
+        System.out.printf("Has the max-heap property for all nodes: %b\n", tree3.hasMaxHeapProperty());
+        System.out.printf("Is a max-heap: %b\n", tree3.isMaxHeap());
+        System.out.println();
+        System.out.println();
+
+        var tree4 = new BinaryTreeNode<>(56);
+
+        tree4.left = new BinaryTreeNode<>(52);
+        tree4.right = new BinaryTreeNode<>(54);
+        tree4.left.left = new BinaryTreeNode<>(50);
+        tree4.left.right = new BinaryTreeNode<>(5);
+        tree4.right.left = new BinaryTreeNode<>(16);
+        tree4.right.right = new BinaryTreeNode<>(20);
+        System.out.println("The tree is complete, and each node is >= its child.");
+        tree4.print();
+
+        System.out.printf("Is a complete tree: %b\n", tree4.isCompleteTree());
+        System.out.printf("Has the max-heap property for all nodes: %b\n", tree4.hasMaxHeapProperty());
+        System.out.printf("Is a max-heap: %b\n", tree4.isMaxHeap());
+        System.out.println();
+        System.out.println();
     }
 
 }
---
+++ samuel-laboration3/src/SamuelDatastructures/BinaryTreeNode.java
@@ -0,0 +1,129 @@
+package SamuelDatastructures;
+
+public class BinaryTreeNode<T extends Comparable<T>> {
+
+    // Have been put as public to enable easy forced modification of the tree for testing
+    public BinaryTreeNode<T> left;
+    public BinaryTreeNode<T> right;
+    T data;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+
+    public void insert(T data) {
+        if (this.data == null) //Empty node
+            this.data = data;
+
+        else if (data.compareTo(this.data) <= 0) { //The incoming data is <= the nodes data
+            if (this.left == null)
+                this.left = new BinaryTreeNode<>(data);
+            else
+                this.left.insert(data); // Recursive call down the left subtree
+
+        } else {
+            if (this.right == null)
+                this.right = new BinaryTreeNode<>(data);
+            else
+                this.right.insert(data); // Recursive call down the right subtree
+        }
+    }
+
+    public boolean isMaxHeap() {
+        return isCompleteTree() && hasMaxHeapProperty();
+    }
+
+    public boolean isCompleteTree() {
+        var queue = new SamuelLinkedListStackQueue<BinaryTreeNode<T>>();
+        boolean missingNode = false;
+        queue.enqueue(this);
+
+        while (!queue.isEmpty()) { // Breadth first traversal
+            var node = queue.deque();
+
+            if (node.left != null) {
+                if (missingNode) // Node with a missing child was encountered before the last node - incomplete
+                    return false;
+                else
+                    queue.enqueue(node.left);
+            } else
+                missingNode = true;
+
+            if (node.right != null) {
+                if (missingNode) // Node with a missing child was encountered before the last node - incomplete
+                    return false;
+                else
+                    queue.enqueue(node.right);
+            } else
+                missingNode = true;
+
+        }
+
+        return true;
+    }
+
+    public boolean hasMaxHeapProperty() {
+        if (this.left != null)
+            if (this.data.compareTo(this.left.data) < 0 || !this.left.hasMaxHeapProperty())
+                return false;
+        if (this.right != null)
+            if (this.data.compareTo(this.right.data) < 0 || !this.right.hasMaxHeapProperty())
+                return false;
+
+        return true;
+    }
+
+    public BinaryTreeNode<T> remove(T data) {
+        return this.recursiveRemove(this, data);
+    }
+
+    private BinaryTreeNode<T> recursiveRemove(BinaryTreeNode<T> node, T data) {
+        if (node == null)
+            return null;
+        else if (data.compareTo(node.data) < 0)
+            node.left = recursiveRemove(node.left, data); // Recursive call down the left subtree
+        else if (data.compareTo(node.data) > 0)
+            node.right = recursiveRemove(node.right, data); // Recursive call down the right subtree
+        else {
+
+            // Case 1: Node has no children
+            if (node.left == null && node.right == null)
+                return null;
+
+                // Case 2: Node has 1 child
+            else if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+
+                //Case 3: Node as 2 children
+            else {
+                node.data = findSubtreeMinimumValue(node.left);
+                node.left = recursiveRemove(node.left, node.data);
+            }
+        }
+
+        return node;
+    }
+
+    private T findSubtreeMinimumValue(BinaryTreeNode<T> node) {
+        return node.right == null ? node.data : findSubtreeMinimumValue(node.right);
+    }
+
+    public void print() {
+        recursivePrint("", true);
+    }
+
+    private void recursivePrint(String indentation, boolean isLeft) {
+        if (this.right != null) {
+            this.right.recursivePrint(indentation + (isLeft ? "│   " : "    "), false);
+        }
+
+        System.out.println(indentation + (isLeft ? "└── " : "┌── ") + data);
+
+        if (this.left != null) {
+            this.left.recursivePrint(indentation + (isLeft ? "    " : "│   "), true);
+        }
+    }
+
+}
