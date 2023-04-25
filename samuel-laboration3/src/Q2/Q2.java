0899fa3018bcd2cbb4d39f870cc828bd684c8167
Samuel Thand <sath2102@student.miun.se>
2023-04-25 11:31:09 +0200

    Started working on identifying near misses

--- samuel-laboration3/src/Q2/Q2.java
+++ samuel-laboration3/src/Q2/Q2.java
@@ -14,23 +14,55 @@ public class Q2 {
 
     public static void main(String[] args) {
 
+        var hashTable = loadWords();
+
+        var userInput = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a word: ");
+
+            var enteredWord = userInput.nextLine().toLowerCase();
+            var nearMisses = checkNearMisses(enteredWord);
+
+            if (hashTable.contains(enteredWord))
+                System.out.println("Exists");
+
+            else if (!nearMisses.isEmpty()) {
+
+                var iter = nearMisses.iterator();
+                do
+                    System.out.println("a");
+                while (iter.hasNext());
+            }
+
+            else
+                System.out.println("Not exist");
+        }
+    }
+
+    private static SLinkedList checkNearMisses(String word) {
+        var nearMisses = new SLinkedList<String>();
+
+        return nearMisses;
+    }
+
+    private static SamuelOpenHashTable loadWords() {
         var dictionary = readFile("samuel-laboration3/src/Q2/words");
 
         int lines = 0;
         var words = new SLinkedList<String>();
         while (dictionary.hasNextLine()) {
-            words.insertAtTail(dictionary.nextLine());
+            words.insertAtTail(dictionary.nextLine().toLowerCase());
             lines++;
         }
 
-        var hashTable = new SamuelOpenHashTable(calculateTableSize(lines)); //TODO appropriate size
+        var hashTable = new SamuelOpenHashTable(calculateTableSize(lines));
         var iterator = words.iterator();
         do
             hashTable.add(iterator.next());
         while (iterator.hasNext());
 
-        System.out.println(hashTable.contains("épée"));
-        System.out.println(hashTable.contains("A(E*FJA(_)EJF9a0"));
+        return hashTable;
     }
 
     public static Scanner readFile(String path) {
