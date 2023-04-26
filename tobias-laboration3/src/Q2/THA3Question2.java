a99bfe4ebe41678e1ba7d6ef2f905871f10f613f
toha2102 <toha2102@student.miun.se>
2023-04-26 13:23:07 +0200

    Q2 hash table

--- tobias-laboration3/src/Q2/THA3Question2.java
+++ tobias-laboration3/src/Q2/THA3Question2.java
@@ -1,4 +1,32 @@
 package Q2;
 
+import util.THOpenHashTable;
+
 public class THA3Question2 {
+    public static void main(String[] args) {
+        partOneDemo();
+    }
+
+    private static void partOneDemo() {
+        THOpenHashTable hashTable = new THOpenHashTable(5);
+        String target = "Dog";
+        System.out.println("Adding A: inserted = " + hashTable.add("A"));
+        System.out.println("Adding A again: inserted = " + hashTable.add("A"));
+        hashTable.add(target);
+        hashTable.add("Ball");
+        hashTable.add("Cat");
+        hashTable.add("Elephant");
+        hashTable.add("Elephant123");
+        hashTable.add("Elephant12345678");
+
+        System.out.println(hashTable);
+        System.out.printf("Size of the hash table is: %d\n", hashTable.size());
+        System.out.println("Contains " + target + ": " + hashTable.contains("A"));
+        System.out.println("Contains B: " + hashTable.contains("B"));
+
+        System.out.println("Removing Dog: removed = " + hashTable.remove(target));
+        System.out.println("Removing Dog again: removed = " + hashTable.remove(target));
+        System.out.println(hashTable);
+        System.out.printf("Size of the hash table is: %d\n", hashTable.size());
+    }
 }
