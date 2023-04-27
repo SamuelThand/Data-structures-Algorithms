99a6d83564ae3708a03e82e949c1da3723cdd97c
Samuel Thand <sath2102@student.miun.se>
2023-04-27 11:30:35 +0200

    added descriptive comments of sorting

--- samuel-laboration3/src/Q3/Q3.java
+++ samuel-laboration3/src/Q3/Q3.java
@@ -1,24 +1,27 @@
 package Q3;
 
-import SamuelDatastructures.SLinkedList;
-import SamuelDatastructures.SamuelOpenHashTable;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q3 {
 
     public static void main(String[] args) {
-
-        //Filename
-        //first, median, random
         System.out.println(args[0] + " " + args[1]);
 
         var unsorted = loadNumbers(args[0]);
+        System.out.println("Unsorted: " + Arrays.toString(unsorted));
+        var sorted = switch (args[1]) {
+            case "first" -> quickSortFirst(unsorted,0,unsorted.length - 1);
+            case "median" -> quickSortMedian(unsorted,0,unsorted.length - 1);
+            case "random" -> quickSortRandom(unsorted,0,unsorted.length - 1);
+            default -> null;
+        };
+
+        System.out.println("Sorted: " + Arrays.toString(sorted));
 
     }
-
 
     private static int[] loadNumbers(String path) {
         var file = readFile(path);
@@ -44,6 +47,55 @@ public class Q3 {
         }
 
         return null;
+    }
+
+    public static int[] quickSortFirst(int[] numbers, int leftIndex, int rightIndex) {
+
+        // Base case - if false, the subarray has been sorted.
+        if (leftIndex < rightIndex) {
+
+            //Divide and conquer - Find the next pivot index by partitioning, then recursively sort the parts split at the pivot.
+            int pivot = partition(numbers, leftIndex, rightIndex);
+            quickSortFirst(numbers, leftIndex, pivot - 1); // First part
+            quickSortFirst(numbers, pivot + 1, rightIndex); // Second part
+        }
+
+        return numbers; // Sorted array
+    }
+
+    public static int[] quickSortMedian(int[] unsorted, int leftIndex, int rightIndex) {
+        return unsorted;
+    }
+
+    public static int[] quickSortRandom(int[] unsorted, int leftIndex, int rightIndex) {
+        return unsorted;
+    }
+
+//    TODO measure operations
+    private static int partition(int[] numbers, int leftIndex, int rightIndex) {
+
+
+        int pivot = numbers[leftIndex];
+        int newPivot = leftIndex;
+        for (int i = leftIndex + 1; i <= rightIndex; i++) {
+
+            // Check if the current element is smaller than the pivot, if so - increment the new pivot and swap the elements.
+            // This will move all elements smaller than the pivot, to before the pivot, making the pivot element sorted.
+            if (numbers[i] < pivot) {
+                newPivot++;
+                swap(numbers, newPivot, i);
+            }
+        }
+        swap(numbers, leftIndex, newPivot);
+
+        return newPivot;
+    }
+
+    //Swap the item of the first index, with the item of the second index.
+    private static void swap(int[] numbers, int firstIndex, int secondIndex) {
+        var temp = numbers[firstIndex];
+        numbers[firstIndex] = numbers[secondIndex];
+        numbers[secondIndex] = temp;
     }
 
 }
